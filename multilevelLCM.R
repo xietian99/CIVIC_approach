9ef10b354159f23821ff9601f2bd9482099a4063
Zhenke Wu <zhenkewu@gmail.com>
2021-06-30 14:46:03 -0400

    edits to make the model run

--- multilevelLCM.R
+++ multilevelLCM.R
@@ -1,5 +1,9 @@
 # MLCM
-setwd("/Volumes/GoogleDrive/My Drive/BiostatMS/CIVIC")
+#setwd("/Volumes/GoogleDrive/My Drive/BiostatMS/CIVIC")
+
+
+rm(list=ls())
+source("simData_cluster.R")
 
 library(rjags)
 library(R2jags)
@@ -8,8 +12,8 @@ library(gtools)
 
 # 1.a define variable
 N = nrow(data)
-G = max(data[,1])
-K = max(data[,2])
+G = max(data[,1]) # this might have problem if some clusters are not sampled; minor issue.
+K = max(data[,2]) # same comment as above.
 J = ncol(data) - 2
 
 
@@ -32,7 +36,7 @@ multilevel_LCM <- function(){
   ## P - A|U
   for (j in 1:J){ 
     for(k in 1:K){
-      P[j,k] ~ dbeta(1,1)
+      P[j,k] ~ dbeta(1,1) # here we are just using fixed hyperparameters (1,1); theoretically, we can also let them be random (I set them to be 5,5 in the simData_cluster.R)
     }
   }
 }
@@ -49,14 +53,14 @@ dat <- list(
 
 # Sincce previously the estimation of PI which is latentclass U | cluster G is around 0.5
 # set initial PI (0.2, 0.8) deviating from (0.5,0.5).
-init.PI = matrix(0.2, nrow = G, ncol = K)
-init.PI[,2] = 1 - init.PI[,1]
+#init.PI = matrix(1/K, nrow = G, ncol = K)
+#init.PI[,2] = 1 - init.PI[,1]
 model.inits <- function(){
-  set.seed(1080)
+  #set.seed(1080) # don't set seed here.
   list(
     P = matrix(runif(J*K, 0,1), nrow = J, ncol = K), # A|U JxK
     U = sample(1:K, size = N, replace = T,  prob = rep(1/K, K)),    # Latent Parameter 1/2/1/2
-    PI =  init.PI
+    PI =  matrix(1/K, nrow = G, ncol = K)
   )  # row sum to 1, randomly
 }
 
@@ -64,7 +68,7 @@ params <- c("P","PI", "U")
 res <- jags(data=dat,inits=model.inits,parameters.to.save = params,
             model.file=multilevel_LCM,n.chains=2,
             # since n.iter 2e4 doesn't converge, set 4e4
-            n.iter=40000,n.burnin=30000)
+            n.iter=1000,n.burnin=500)
 res.mcmc = as.mcmc(res)
 real.P = P
 real.PI = PI
@@ -89,13 +93,17 @@ xyplot(
 
 ## 3.2.1 For P: J*K
 df.P = as.data.frame(res.mcmc[[1]][,2:(K*J + 1)]) 
-colMeans(df.par)
+colMeans(df.P)
 store.P = real.P
-store.P[2,] = real.P[10,]
-store.P[10,] = real.P[2,]
-df.P = melt(df.par, variable_name = "parameter")
+# store.P[2,] = real.P[10,]
+# store.P[10,] = real.P[2,]
+df.P = melt(df.P, variable_name = "parameter")
 
-df.P$TrueVal = rep(t(store.P), each = N)
+df.P$parameter <- factor(df.P$parameter,levels=c("P[1,1]",  "P[1,2]",  "P[2,1]",  "P[2,2]",  "P[3,1]",  "P[3,2]",  "P[4,1]",  "P[4,2]",  "P[5,1]", 
+                                                 "P[5,2]","P[6,1]","P[6,2]","P[7,1]","P[7,2]","P[8,1]","P[8,2]","P[9,1]","P[9,2]","P[10,1]","P[10,2]"))
+
+df.P$TrueVal = rep(c(t(store.P)), each = nrow(res.mcmc[[1]])) ## this needs to be modified to this.
+
 df.P %>% ggplot(aes(x = parameter, y = value)) + geom_violin(scale = "width") + 
   stat_summary(fun.data = median_hilow, geom="pointrange") + 
   geom_point(aes(x = parameter, y = TrueVal), shape = 4, color = "#56B4E9") + 
@@ -104,11 +112,15 @@ df.P %>% ggplot(aes(x = parameter, y = value)) + geom_violin(scale = "width") +
 ## 3.2.2 For PI: G*K
 df.PI = as.data.frame(res.mcmc[[1]][,seq(K*J+2, K*J+G*K,length.out = G)])   
 store.PI = real.PI
-store.PI[2] = real.PI[10]
-store.PI[10] = real.PI[2]
+# store.PI[2,] = real.PI[10,]
+# store.PI[10,] = real.PI[2,]
 df.PI = melt(df.PI, variable_name = "parameter")
-df.PI$TrueVal = rep(store.PI, each = N)
+df.PI$parameter <- factor(df.PI$parameter,levels=c("PI[1,1]","PI[2,1]",  "PI[3,1]",  "PI[4,1]",  "PI[5,1]",  "PI[6,1]",  "PI[7,1]",  "PI[8,1]",  "PI[9,1]","PI[10,1]" ))
+
+df.PI$TrueVal = rep(store.PI[,1], each = nrow(res.mcmc[[1]])) ## modified.
+
 df.PI %>% ggplot(aes(x = parameter, y = value)) + geom_violin(scale = "width") + 
   stat_summary(fun.data = median_hilow, geom="pointrange") + 
-  geom_point(aes(x = parameter, y = TrueVal), shape = 4, color = "#56B4E9") + 
+  geom_point(aes(x = parameter, y = TrueVal), shape = 4, color = "#56B4E9") + # this is PI[g,1]
+  #geom_point(aes(x = parameter, y = 1-TrueVal), shape = 4, color = "red") + # this is for 1-PI[g,1]
   ggtitle(paste("Multi-level latent class model: G = ",G, ", K = ", K, ", N = ", N, ", J = ", J))
--- simData_cluster.R
+++ simData_cluster.R
@@ -3,10 +3,18 @@
 ## 1. Define Parameter, Generate PI/P
 G = 10; K = 2; J = 10; N = 1000
 set.seed(1080)
-PI = matrix(runif(G*K, 0, 1), nrow = G, ncol = K)
-PI[,K] = 1 - rowSums(PI[,1:(K-1),drop = F])
-P = matrix(0.2, nrow = J, ncol = K)
-P[,2] = 1 - P[,1]
+# PI = matrix(runif(G*K, 0, 1), nrow = G, ncol = K)
+# PI[,K] = 1 - rowSums(PI[,1:(K-1),drop = F])
+
+library(gtools)
+#PI <- rdirichlet(G,c(1,1))  # ZW: sometimes the true PI's are extreme, e.g., 0.95, so it is hard to estimate the proportion for the other class because it is rare (0.05 chance) to have observations from the second class.
+PI <- matrix(0.5,nrow=G,ncol=2)
+
+#P = matrix(0.2, nrow = J, ncol = K)
+#P[,2] = 1 - P[,1]
+
+P <- matrix(c(0.2,0.9),nrow=J,ncol=K,byrow=TRUE)
+
 ## 2. Generate Dataset
 ### 1 Cluster  + 1 latent class + J causes
 data = matrix(0, nrow = N, ncol = 2 + J ) 
@@ -17,7 +25,7 @@ for (i in 1:N){
   data[i,2] = sample(1:K, 1, prob = PI[cls_i,,drop = F] ) 
   # generate causes
   for (j in 1:J){
-    data[i,j+2] = rbinom(n=1, size = 1, prob = P[j, data[,2]])
+    data[i,j+2] = rbinom(n=1, size = 1, prob = P[j, data[i,2]])
   }
   # The outcome data to be generated 
 }
